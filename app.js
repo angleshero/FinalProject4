88c639f225a32816466f31ef068920e32cbc56a7
Tommy <tommylcowart@gmail.com>
2025-10-22 11:10:11 -0500

    last change

--- app.js
+++ app.js
@@ -7,15 +7,27 @@ console.log (moviesWrapper)
 
 function searchChange(event) {
      renderMovies(event.target.value)  
-     searchName,innerHTML = event.target.value    
+     searchName.innerHTML = event.target.value    
 }
+
+let currentMovies = [];
+
+//rendering movies 
 
 async function renderMovies(searchTerm) {
 
-const response = await fetch (`https://www.omdbapi.com/?s=${searchTerm}&apikey=21463539&s`)
-const data = await response.json()
-const moviesArr = data.Search
-moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
+const response = await fetch (`https://www.omdbapi.com/?s=${searchTerm}&apikey=21463539&s`);
+const data = await response.json();
+currentMovies = data.Search;
+displayMovies(currentMovies);
+
+}
+
+
+//displaying movies 
+
+function displayMovies(movieList) {
+     moviesWrapper.innerHTML = movieList.slice(0, 6).map((movie) => {
     return `
     <div class="movie">
     <img src=${movie.Poster} alt="" />
@@ -24,8 +36,24 @@ moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
     <button>Learn More</button>
     </div> 
     `
-       
+       ;
     }).join ('');
 }
 
 
+//sorting movies
+
+
+function sortChange(event) {
+    const sortOption = event.target.value;
+
+    let sortedMovies = [...currentMovies];
+
+    if (sortOption === "newest") {
+        sortedMovies.sort((a, b) => b.Year - a.Year);
+    } else if (sortOption === "oldest") {
+        sortedMovies.sort((a, b) => a.Year - b.Year);
+}
+displayMovies(sortedMovies);
+
+}
